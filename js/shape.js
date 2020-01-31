671d49525cb689a048662205f19be4ef37c03faa
huahua <huazixu1998@gmail.com>
2020-01-31 13:54:28 +0800

    1024纪念

--- js/shape.js
+++ js/shape.js
@@ -17,7 +17,7 @@ function Shape(x, y, texte) {
     if (IsPC()) {
         this.size = 180;
     } else {
-        this.size = 180;
+        this.size = 140;
     }
     this.text = texte;
     this.placement = [];
